422c99aeaa26623bef880c06e0e1b22a6b1461b3
sogol <sogollnoori@gmail.com>
2025-05-02 20:06:43 -0700

    one

--- stardew_valley/src/main/java/org/example/models/User.java
+++ stardew_valley/src/main/java/org/example/models/User.java
@@ -26,6 +26,34 @@ public class User {
         return username;
     }
 
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
     public String getSecurityQuestion() {
         return securityQuestion;
     }
