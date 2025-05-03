d933d14f1618b8ad1df3a8db32b9822c32a512db
Parnia_FEA <ParniaDabbagh238@gmail.com>
2025-05-03 14:50:09 +0330

    Profile Menu Commands and Login Menu Commands are implemented

---
+++ stardew_valley/src/main/java/org/example/models/enums/LoginMenuCommands.java
@@ -0,0 +1,28 @@
+package org.example.models.enums;
+
+import java.util.regex.Matcher;
+
+public enum LoginMenuCommands implements org.example.models.enums.Command {
+    MenuEnter("\\s*menu\\s+enter\\s+(?<menuName>.+?)\\s*"),
+    MenuExit("\\s*menu\\s+exit\\s*"),
+    ShowCurrentMenu("\\s*show\\s+current\\s+menu\\s*"),
+    Login("\\s*login\\s+-u\\s+(?<username>.+)\\s+-p\\s+(?<password>.+)\\s+(?<stayLoggedIn>.*)\\s*"),
+    ForgetPassword("\\s*forget\\s+password\\s+-u\\s+(?<username>.+)\\s*"),
+    Answer("\\s*answer\\s+-a\\s+(?<answer>.+)\\s*")
+    ;
+
+
+    private final String pattern;
+    LoginMenuCommands(String pattern) {
+        this.pattern = pattern;
+    }
+    @Override
+    public String getPattern() {
+        return "";
+    }
+
+    @Override
+    public Matcher getMatcher(String input) {
+        return Command.super.getMatcher(input);
+    }
+}
---
+++ stardew_valley/src/main/java/org/example/models/enums/ProfileMenuCommands.java
@@ -0,0 +1,27 @@
+package org.example.models.enums;
+
+import java.util.regex.Matcher;
+
+public enum ProfileMenuCommands implements Command{
+    ChangeUsername("\\s*change\\s+username\\s+-u\\s+(?<username>.+)\\s*"),
+    ChangeNickname("\\s*change\\s+nickname\\s+-u\\s+(?<nickname>.+)\\s*"),
+    ChangeEmail("\\s+change\\s+email\\s+-e\\s+(?<email>.+)\\s*"),
+    ChangePassword("\\s*change\\s+password\\s+-p\\s+(?<newPassword>.+)\\s+-o\\s+(?<oldPassword>.+)\\s*"),
+    UserInfo("\\s*user\\s+info\\s*"),
+    ;
+
+
+    private final String pattern;
+    ProfileMenuCommands(String pattern) {
+        this.pattern = pattern;
+    }
+    @Override
+    public String getPattern() {
+        return "";
+    }
+
+    @Override
+    public Matcher getMatcher(String input) {
+        return Command.super.getMatcher(input);
+    }
+}
