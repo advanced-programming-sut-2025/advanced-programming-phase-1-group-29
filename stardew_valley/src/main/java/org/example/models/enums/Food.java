d08c882194bc4b786770206c49ca66322853801c
sogol <sogollnoori@gmail.com>
2025-05-10 13:24:21 -0700

    Fish Enum+

--- stardew_valley/src/main/java/org/example/models/enums/Food.java
+++ stardew_valley/src/main/java/org/example/models/enums/Food.java
@@ -1,36 +1,41 @@
 package org.example.models.enums;
 
+import java.util.ArrayList;
+
 public enum Food {
-    FriedEgg (50, 35),
-    BakedFish (75, 100),
-    Salad (113, 110),
-    Omelet (100, 125),
-    PumpkinPie (225, 385),
-    Spaghetti (75, 120),
-    Pizza (150, 300),
-    Tortilla (50, 50),
-    MakiRoll (100, 220),
-    TripleShotEspresso (200, 450),
-    Cookie (90, 140),
-    HashBrowns (90, 120),
-    Pancakes (90, 80),
-    FruitSalad (263, 450),
-    RedPlate (240, 400),
-    Bread (50, 60),
-    SalmonDinner (125, 300),
-    VegetableMedley (165, 120),
-    FarmersLunch (200, 150),
-    SurvivalBurger (125, 180),
-    DishOfTheSea (150, 220),
-    SeaFormPudding (175, 300),
-    minersTreat (125, 200),
+    FriedEgg ("Fried egg", 50, 35),
+    BakedFish ("Baked Fish", 75, 100),
+    Salad ("Salad", 113, 110),
+    Omelet ("Omelet", 100, 125),
+    PumpkinPie ("pumpkin pie", 225, 385),
+    Spaghetti ("spaghetti", 75, 120),
+    Pizza ("pizza", 150, 300),
+    Tortilla ("Tortilla", 50, 50),
+    MakiRoll ("Maki Roll", 100, 220),
+    TripleShotEspresso ("Triple Shot Espresso", 200, 450),
+    Cookie ("Cookie", 90, 140),
+    HashBrowns ("hash browns", 90, 120),
+    Pancakes ("pancakes", 90, 80),
+    FruitSalad ("fruit salad", 263, 450),
+    RedPlate ("red plate", 240, 400),
+    Bread ("bread", 50, 60),
+    SalmonDinner ("salmon dinner", 125, 300),
+    VegetableMedley ("vegetable medley ", 165, 120),
+    FarmersLunch ("farmer's lunch", 200, 150),
+    SurvivalBurger ("survival burger", 125, 180),
+    DishOfTheSea ("dish O' the Sea", 150, 220),
+    SeaFormPudding ("seaform Pudding", 175, 300),
+    minersTreat ("miner's treat", 125, 200),
     ;
 
+    private final String name;
     private final int energy;
     private final int sellPrice;
+    //private final ArrayList<Food> ingridients;
 
 
-    Food(int energy, int sellPrice) {
+    Food(String  name, int energy, int sellPrice) {
+        this.name = name;
         this.energy = energy;
         this.sellPrice = sellPrice;
     }
