c508a3b60aa00ecc44dcf9a907ba3d4bfbd2cf9b
Parnia_FEA <ParniaDabbagh238@gmail.com>
2025-05-11 13:23:39 +0330

    TreeEnum, TreeSeedEnum, FruitEnum are implemented

--- stardew_valley/src/main/java/org/example/models/enums/FruitEnum.java
+++ stardew_valley/src/main/java/org/example/models/enums/FruitEnum.java
@@ -1,7 +1,20 @@
 package org.example.models.enums;
 
 public enum FruitEnum {
-    ;
+    APRICOT("Apricot", 59, true, 38),
+    CHERRY("Cherry", 80, true, 38),
+    BANANA("Banana", 150, true, 75),
+    MANGO("Mango", 130, true, 100),
+    ORANGE("Orange", 100, true, 38),
+    PEACH("Peach", 140, true, 38),
+    APPLE("Apple", 100, true, 38),
+    POMEGRANATE("Pomegranate", 140, true, 38),
+    OAK_RESIN("Oak Resin", 150, false, null),
+    MAPLE_SYRUP("Maple Syrup", 200, false, null),
+    PINE_TAR("Pine Tar", 100, false, null),
+    SAP("Sap", 2, true, -2),
+    COMMON_MUSHROOM("Common Mushroom", 40, true, 38),
+    MYSTIC_SYRUP("Mystic Syrup", 1000, true, 500);
     private final String name;
     private final int baseSellPrice;
     private final boolean isEdible;
@@ -12,5 +25,21 @@ public enum FruitEnum {
         this.baseSellPrice = baseSellPrice;
         this.isEdible = isEdible;
         this.energy = energy;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getBaseSellPrice() {
+        return baseSellPrice;
+    }
+
+    public boolean isEdible() {
+        return isEdible;
+    }
+
+    public Integer getEnergy() {
+        return energy;
     }
 }
--- stardew_valley/src/main/java/org/example/models/enums/TreeEnum.java
+++ stardew_valley/src/main/java/org/example/models/enums/TreeEnum.java
@@ -4,7 +4,62 @@ import java.util.ArrayList;
 import java.util.List;
 
 public enum TreeEnum {
-    ;
+    APRICOT_TREE(
+            "Apricot Tree", TreeSeedEnum.APRICOT_SAPLING, FruitEnum.APRICOT,
+            1, new ArrayList<>(List.of("Spring"))
+    ),
+    CHERRY_TREE(
+            "Cherry Tree", TreeSeedEnum.CHERRY_SAPLING, FruitEnum.CHERRY,
+            1, new ArrayList<>(List.of("Spring"))
+    ),
+    BANANA_TREE(
+            "Banana Tree", TreeSeedEnum.BANANA_SAPLING, FruitEnum.BANANA,
+            1, new ArrayList<>(List.of("Summer"))
+    ),
+    MANGO_TREE(
+            "Mango Tree", TreeSeedEnum.MANGO_SAPLING, FruitEnum.MANGO,
+            1, new ArrayList<>(List.of("Summer"))
+    ),
+    ORANGE_TREE(
+            "Orange Tree", TreeSeedEnum.ORANGE_SAPLING, FruitEnum.ORANGE,
+            1, new ArrayList<>(List.of("Summer"))
+    ),
+    PEACH_TREE(
+            "Peach Tree", TreeSeedEnum.PEACH_SAPLING, FruitEnum.PEACH,
+            1, new ArrayList<>(List.of("Summer"))
+    ),
+    APPLE_TREE(
+            "Apple Tree", TreeSeedEnum.APPLE_SAPLING, FruitEnum.APPLE,
+            1, new ArrayList<>(List.of("Fall"))
+    ),
+    POMEGRANATE_TREE(
+            "Pomegranate Tree", TreeSeedEnum.POMEGRANATE_SAPLING, FruitEnum.POMEGRANATE,
+            1, new ArrayList<>(List.of("Fall"))
+    ),
+    OAK_TREE(
+            "Oak Tree", TreeSeedEnum.ACORN, FruitEnum.OAK_RESIN,
+            7, new ArrayList<>(List.of("Spring", "Summer", "Fall", "Winter"))
+    ),
+    MAPLE_TREE(
+            "Maple Tree", TreeSeedEnum.MAPLE_SEED, FruitEnum.MAPLE_SYRUP,
+            9, new ArrayList<>(List.of("Spring", "Summer", "Fall", "Winter"))
+    ),
+    PINE_TREE(
+            "Pine Tree", TreeSeedEnum.PINE_CONE, FruitEnum.PINE_TAR,
+            5, new ArrayList<>(List.of("Spring", "Summer", "Fall", "Winter"))
+    ),
+    MAHOGANY_TREE(
+            "Mahogany Tree", TreeSeedEnum.MAHOGANY_SEED, FruitEnum.SAP,
+            1, new ArrayList<>(List.of("Spring", "Summer", "Fall", "Winter"))
+    ),
+    MUSHROOM_TREE(
+            "Mushroom Tree", TreeSeedEnum.MUSHROOM_TREE_SEED, FruitEnum.COMMON_MUSHROOM,
+            1, new ArrayList<>(List.of("Spring", "Summer", "Fall", "Winter"))
+    ),
+    MYSTIC_TREE(
+            "Mystic Tree", TreeSeedEnum.MYSTIC_TREE_SEED, FruitEnum.MYSTIC_SYRUP,
+            7, new ArrayList<>(List.of("Spring", "Summer", "Fall", "Winter"))
+    );
     private final String name;
     private final TreeSeedEnum source;
     private final ArrayList<Integer> stages = new ArrayList<>(List.of(7, 7, 7, 7));
@@ -19,5 +74,33 @@ public enum TreeEnum {
         this.fruit = fruit;
         this.fruitHarvestCycle = fruitHarvestCycle;
         this.seasons = seasons;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public TreeSeedEnum getSource() {
+        return source;
+    }
+
+    public ArrayList<Integer> getStages() {
+        return stages;
+    }
+
+    public int getTotalHarvestTime() {
+        return totalHarvestTime;
+    }
+
+    public FruitEnum getFruit() {
+        return fruit;
+    }
+
+    public int getFruitHarvestCycle() {
+        return fruitHarvestCycle;
+    }
+
+    public ArrayList<String> getSeasons() {
+        return seasons;
     }
 }
