9fb6fc8fdfc0685d0b96542113a9eb55ec70e96e
Setytousi <setytousi@gmail.com>
2025-05-11 13:46:31 +0330

    added carpenters shop

--- stardew_valley/src/main/java/org/example/models/CarpentersShop.java
+++ stardew_valley/src/main/java/org/example/models/CarpentersShop.java
@@ -5,4 +5,16 @@ public class CarpentersShop extends Store{
     public Result welcomeMessage() {
         return new Result(true, "Welcome to Carpenter's shop!");
     }
+    public CarpentersShop() {
+        super();
+        addProducts();
+    }
+    public void resetCapacity(){
+        super.resetProducts();
+    }
+    private void addProducts(){
+        super.addProduct(new InventoryItem("Wood", 10), 100000000);
+        super.addProduct(new InventoryItem("Stone", 20), 100000000);
+    }
+
 }
