d783a0b74576ed610c58c72955b7ab0f20271daf
Setytousi <setytousi@gmail.com>
2025-05-11 13:07:47 +0330

    added blacksmith products

--- stardew_valley/src/main/java/org/example/models/FishShop.java
+++ stardew_valley/src/main/java/org/example/models/FishShop.java
@@ -1,4 +1,8 @@
 package org.example.models;
 
 public class FishShop extends Store{
+    @Override
+    public Result welcomeMessage() {
+        return new Result(true, "Welcome to Fish Shop!");
+    }
 }
