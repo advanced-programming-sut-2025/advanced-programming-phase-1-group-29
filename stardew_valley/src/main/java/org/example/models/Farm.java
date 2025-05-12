87055d4629abb29a6a905870b35ea43952cc7eaa
Setytousi <setytousi@gmail.com>
2025-05-12 11:11:30 +0330

    good morning

--- stardew_valley/src/main/java/org/example/models/Farm.java
+++ stardew_valley/src/main/java/org/example/models/Farm.java
@@ -62,4 +62,5 @@ public class Farm extends Objectt {
         }
         return null;
     }
+
 }
--- stardew_valley/src/main/java/org/example/models/Village.java
+++ stardew_valley/src/main/java/org/example/models/Village.java
@@ -1,5 +1,6 @@
 package org.example.models;
 
+import javax.xml.stream.events.StartDocument;
 import java.util.ArrayList;
 
 public class Village{
@@ -8,64 +9,64 @@ public class Village{
     private int xStart;
     private int yStart;
     private ArrayList<Objectt> objects = new ArrayList<>();
-    private BlackSmithStore blackSmithStore;
-    private CarpentersShop carpentersShop;
-    private FishShop fishShop;
-    private JojaMartStore jojaMartStore;
-    private MarniesRanch marniesRanch;
-    private PierresGeneralStore pierresGeneralStore;
-    private TheStardropSaloon stardropSaloon;
 
     public Village(){
-        blackSmithStore = new BlackSmithStore();
+        BlackSmithStore blackSmithStore = new BlackSmithStore();
         ArrayList<Tile> blackSmithTiles = new ArrayList<>();
         for (int i = 20; i < 30; i++){
             for (int j = 20; j < 30; j++){
                 blackSmithTiles.add(new Tile('B', i, j));
             }
         }
-        carpentersShop = new CarpentersShop();
+        objects.add(blackSmithStore);
+        CarpentersShop carpentersShop = new CarpentersShop();
         ArrayList<Tile> carpentersTiles = new ArrayList<>();
         for (int i = 5; i < 10; i++){
             for (int j = 35; j < 50; j++){
                 carpentersTiles.add(new Tile('c', i, j));
             }
         }
-        fishShop = new FishShop();
+        objects.add(carpentersShop);
+        FishShop fishShop = new FishShop();
         ArrayList<Tile> fishTiles = new ArrayList<>();
         for (int i = 10; i < 20; i++){
             for (int j = 5; j < 10; j++){
                 fishTiles.add(new Tile('f', i, j));
             }
         }
-        jojaMartStore = new JojaMartStore();
+        objects.add(fishShop);
+        JojaMartStore jojaMartStore = new JojaMartStore();
         ArrayList<Tile> jojaMartTiles = new ArrayList<>();
         for (int i = 35; i < 45; i++){
             for (int j = 5; j < 15; j++){
                 jojaMartTiles.add(new Tile('J', i, j));
             }
         }
-        marniesRanch = new MarniesRanch();
+        objects.add(jojaMartStore);
+        MarniesRanch marniesRanch = new MarniesRanch();
         ArrayList<Tile> marniesRanchTiles = new ArrayList<>();
         for (int i = 50; i < 55; i++){
             for (int j = 20; j < 35; j++){
                 marniesRanchTiles.add(new Tile('M', i, j));
             }
         }
-        pierresGeneralStore = new PierresGeneralStore();
+        objects.add(marniesRanch);
+        PierresGeneralStore pierresGeneralStore = new PierresGeneralStore();
         ArrayList<Tile> pierresGeneralTiles = new ArrayList<>();
         for (int i = 30; i < 50; i++){
             for (int j = 40; j < 45; j++){
                 pierresGeneralTiles.add(new Tile('P', i, j));
             }
         }
-        stardropSaloon = new TheStardropSaloon();
+        objects.add(pierresGeneralStore);
+        TheStardropSaloon stardropSaloon = new TheStardropSaloon();
         ArrayList<Tile> stardropSaloonTiles = new ArrayList<>();
         for (int i = 15; i < 25; i++){
             for (int j = 45; j < 55; j++){
                 stardropSaloonTiles.add(new Tile('S', i, j));
             }
         }
+        objects.add(stardropSaloon);
     }
 
     public static int getXRange() {
@@ -100,31 +101,19 @@ public class Village{
         this.objects.add(object);
     }
 
-    public BlackSmithStore getBlackSmithStore() {
-        return blackSmithStore;
-    }
-
-    public CarpentersShop getCarpentersShop() {
-        return carpentersShop;
-    }
-
-    public FishShop getFishShop() {
-        return fishShop;
-    }
-
-    public JojaMartStore getJojaMartStore() {
-        return jojaMartStore;
-    }
-
-    public MarniesRanch getMarniesRanch() {
-        return marniesRanch;
-    }
-
-    public PierresGeneralStore getPierresGeneralStore() {
-        return pierresGeneralStore;
-    }
-
-    public TheStardropSaloon getStardropSaloon() {
-        return stardropSaloon;
+    public Objectt getCurrentPlace(){
+        Player currentPlayer = App.getCurrentGame().getCurrentPlayer();
+        for (Objectt object : objects){
+            boolean check = false;
+            for (Tile tile : object.getTiles()){
+                if (tile.getX() == currentPlayer.getX() && tile.getY() == currentPlayer.getY()){
+                    check = true;
+                }
+            }
+            if (check){
+                return object;
+            }
+        }
+        return null;
     }
 }
