27135a20a60413d7c3e080c0fe2603888c5d8b47
Parnia_FEA <ParniaDabbagh238@gmail.com>
2025-05-16 01:40:27 +0330

    fishing pole completed

--- stardew_valley/src/main/java/org/example/models/FishingPole.java
+++ stardew_valley/src/main/java/org/example/models/FishingPole.java
@@ -1,7 +1,10 @@
 package org.example.models;
 
 
+import org.example.models.enums.FishEnum;
 import org.example.models.enums.FishingPoleType;
+
+import java.util.Random;
 
 public class FishingPole extends Tool{
     private FishingPoleType type;
@@ -20,9 +23,48 @@ public class FishingPole extends Tool{
         return type;
     }
 
+    private boolean nearTheLake() {
+        Player player = App.getCurrentGame().getCurrentPlayer();
+        Farm farm = App.getCurrentGame().getCurrentFarm();
+        for (Objectt objectt : farm.getObjects()) {
+            if(objectt instanceof Lake lake) {
+                int x = player.getX();
+                int y = player.getY();
+                for (Tile tile : lake.getTiles()) {
+                    if (Math.abs(tile.getX() - x) <= 1 && Math.abs(tile.getY() - y) <= 1) return true;
+                }
+            }
+        }
+        return false;
+    }
+
     @Override
     public Result useTool(int x, int y) {
-        return null;
+        Game game = App.getCurrentGame();
+        Player player = App.getCurrentGame().getCurrentPlayer();
+        Inventory inventory = player.getInventory();
+        if(!nearTheLake()){
+            return new Result(false, "You are not near the lake");
+        }
+        Random rand = new Random();
+        int amount = Math.min((int) (Math.ceil(rand.nextDouble(2) * game.getWeather().getFishingRate() * (player.getFishingLevel() + 2))), 6);
+        double quality = rand.nextDouble(2) * (player.getFishingLevel() + 2) * type.getQuantityRate() / (7 - amount);
+        amount = Math.min(amount, inventory.getCapacity());
+        int actualAmount = 0;
+        for (int i = 0; i < amount; i++) {
+            if (player.getInventory().getCapacity() > 0) {
+                FishEnum fishEnum = FishEnum.getRandomFish(game.getCurrentTime().getSeason(), player.getFishingLevel() == 4);
+                InventoryItem fish = new InventoryItem(fishEnum.getName(), 0);
+                player.setEnergy(player.getEnergy() - this.applyWeatherOnEnergyConsumption(this.getEnergyConsumption()));
+                fish.setQuality(quality);
+                inventory.addInventoryItem(fish, 1);
+                actualAmount++;
+            }
+            else {
+                break;
+            }
+        }
+        return new Result(true, "You caught " + actualAmount);
     }
 
     @Override
