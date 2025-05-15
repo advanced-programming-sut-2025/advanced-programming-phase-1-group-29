28303b138c67fbc2b9bbb8f0a23106c33af208f1
Parnia_FEA <ParniaDabbagh238@gmail.com>
2025-05-15 18:04:22 +0330

    pickaxe

--- stardew_valley/src/main/java/org/example/models/enums/Weather.java
+++ stardew_valley/src/main/java/org/example/models/enums/Weather.java
@@ -1,19 +1,25 @@
 package org.example.models.enums;
 
 public enum Weather {
-    SUNNY (1.5),
-    RAINY (1.2),
-    STORMY (0.5),
-    SNOWY (1),
+    SUNNY (1.5, 1),
+    RAINY (1.2, 1.5),
+    STORMY (0.5, 1.5),
+    SNOWY (1, 2),
     ;
 
     private final double fishingRate;
+    private final double energyConsumption;
 
-    private Weather(double fishingRate) {
+    Weather(double fishingRate, double energyConsumption) {
         this.fishingRate = fishingRate;
+        this.energyConsumption = energyConsumption;
     }
 
     public double getFishingRate() {
         return fishingRate;
+    }
+
+    public double getEnergyConsumption() {
+        return energyConsumption;
     }
 }
