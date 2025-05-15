bb7ed206264afb7fcd4b408cbd924a2ba1da0bb0
Parnia_FEA <ParniaDabbagh238@gmail.com>
2025-05-16 00:43:05 +0330

    barn

--- stardew_valley/src/main/java/org/example/models/Barn.java
+++ stardew_valley/src/main/java/org/example/models/Barn.java
@@ -5,4 +5,15 @@ import java.util.ArrayList;
 public class Barn extends Objectt{
     private ArrayList<Animal> animals = new ArrayList<>();
 
+    public ArrayList<Animal> getAnimals() {
+        return animals;
+    }
+
+    public void setAnimals(ArrayList<Animal> animals) {
+        this.animals = animals;
+    }
+
+    public void addAnimal(Animal animal) {
+        animals.add(animal);
+    }
 }
