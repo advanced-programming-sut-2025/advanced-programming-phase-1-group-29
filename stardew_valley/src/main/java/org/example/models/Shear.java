bb6c1e0efd7f0049f5b0b220e65a821ac222691f
Parnia_FEA <ParniaDabbagh238@gmail.com>
2025-05-16 01:26:52 +0330

    shear milk pail

--- stardew_valley/src/main/java/org/example/models/Shear.java
+++ stardew_valley/src/main/java/org/example/models/Shear.java
@@ -1,5 +1,7 @@
 package org.example.models;
 
+import java.util.ArrayList;
+
 public class Shear extends Tool{
 
     public Shear(String name) {
@@ -13,6 +15,29 @@ public class Shear extends Tool{
 
     @Override
     public Result useTool(int x, int y) {
-        return null;
+        Player player = App.getCurrentGame().players.get(App.getCurrentGame().getTurn());
+        Farm farm = App.getCurrentGame().getMap().getFarms().get(App.getCurrentGame().getTurn());
+        for (Objectt object : farm.getObjects()) {
+            if (object instanceof Barn barn) {
+                if (object.getTiles().getFirst().getX() == x && object.getTiles().getFirst().getY() == y) {
+                    for (Animal animal : barn.getAnimals()) {
+                        ArrayList<InventoryItem> toBeRemoved = new ArrayList<>();
+                        for (int i = 0; i < Math.min(animal.getProducts().size(), player.getInventory().getCapacity()); i++) {
+                            if (animal.getProducts().get(i).getName().equalsIgnoreCase("Wool")) {
+                                player.getInventory().addInventoryItem(animal.getProducts().get(i), 1);
+                                player.setEnergy(player.getEnergy() - this.applyWeatherOnEnergyConsumption(this.getEnergyConsumption()));
+                                toBeRemoved.add(animal.getProducts().get(i));
+                            }
+                        }
+                        for (InventoryItem inventoryItem : toBeRemoved) {
+                            animal.getProducts().remove(inventoryItem);
+                        }
+                    }
+                    return new Result(true, "The shear is used successfully.");
+                }
+            }
+        }
+        player.setEnergy(player.getEnergy() - this.applyWeatherOnEnergyConsumption(this.getEnergyConsumption()));
+        return new Result(false, "The shear can't be used in this direction.");
     }
 }
